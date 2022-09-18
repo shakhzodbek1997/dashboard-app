6384904d73f6aa7cb682bd8ff3f7607128b38710
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-09-18 23:53:48 +0200

    Context API

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-
-ReactDOM.render( <App />, document.getElementById('root'));
+import { ContextProvider } from './contexts/ContextProvider';
+ReactDOM.render(
+    <ContextProvider>
+        <App />, 
+    </ContextProvider>,
+    document.getElementById('root')
+);
