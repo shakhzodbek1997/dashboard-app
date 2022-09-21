5910c69e7a58d5570ea00eb156dd68b6587faa71
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-09-21 11:41:05 +0200

    Calendar is done with syncfusion power

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
               {/* Apps */}
               <Route path='/kanban' element={<Kanban /> } />
               <Route path='/editor' element={<Editor/> } />
-              <Route path='/calendor' element={<Calendar /> } />
+              <Route path='/calendar' element={<Calendar /> } />
               <Route path='/color-picker' element={<ColorPicker /> } />
               
               {/* Charts */}
